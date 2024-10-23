c82d19ded159c61ac85bcf975c257245bd08cb73
philip517 <philiphilip517@gmail.com>
2024-10-23 22:49:49 +0200

    nothing done

--- refresh.php
+++ refresh.php
@@ -12,24 +12,5 @@ if(isset($_SESSION['website_id'])){
     header("Location:home.php");
     
 }
-// if(isset($_GET['id'])){
-    
-//     echo $_GET['id'];
 
-// }else{
-//     echo 'No id found';
-// }
-
-// if(isset($_GET['id'])){
-//     $id=$_GET['id'];
-// }
-
-// $sql = "SELECT * FROM website WHERE id=$id";
-// $stmt = $pdo->prepare($sql);
-// $stmt->execute();
-
-// // Fetch all results into an associative array
-// $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-// vardump($data);
 ?>
